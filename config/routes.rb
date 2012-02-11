0893d8275c7dd65d1fd6d3e979455b04e7bf8b1c
muharer <muharer@gmail.com>
2012-02-12 00:27:59 +0200

    static css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "static_pages/home"
 
   get "static_pages/help"
 
   get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
